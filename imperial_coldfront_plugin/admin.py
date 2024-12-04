29ee71b422a735d7317a1a517184073745987a38
James Paul Turner <james.turner@imperial.ac.uk>
2024-12-04 11:57:16 +0000

    Add ResearchGroup to admin interface.

--- imperial_coldfront_plugin/admin.py
+++ imperial_coldfront_plugin/admin.py
@@ -2,7 +2,16 @@
 
 from django.contrib import admin
 
-from .models import GroupMembership
+from .models import GroupMembership, ResearchGroup
+
+
+class ResearchGroupAdmin(admin.ModelAdmin):
+    """Admin configuration for the ResearchGroup model."""
+
+    pass
+
+
+admin.site.register(ResearchGroup, ResearchGroupAdmin)
 
 
 class GroupMembershipAdmin(admin.ModelAdmin):
