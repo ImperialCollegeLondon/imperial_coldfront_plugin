9341a407c40334e3577e8e7523cb82fe61508a47
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-12-15 02:43:43 +0000

    Add UnixUID page to admin

--- imperial_coldfront_plugin/admin.py
+++ imperial_coldfront_plugin/admin.py
@@ -2,22 +2,25 @@
 
 from django.contrib import admin
 
-from .models import GroupMembership, ResearchGroup
+from .models import GroupMembership, ResearchGroup, UnixUID
 
 
+@admin.register(ResearchGroup)
 class ResearchGroupAdmin(admin.ModelAdmin):
     """Admin configuration for the ResearchGroup model."""
 
     list_display = ("owner", "gid", "name")
 
 
-admin.site.register(ResearchGroup, ResearchGroupAdmin)
-
-
+@admin.register(GroupMembership)
 class GroupMembershipAdmin(admin.ModelAdmin):
     """Admin configuration for the GroupMembership model."""
 
     pass
 
 
-admin.site.register(GroupMembership, GroupMembershipAdmin)
+@admin.register(UnixUID)
+class UnixUIDAdmin(admin.ModelAdmin):
+    """Admin configuration for the UnixUID model."""
+
+    list_display = ("identifier", "user")
