1775a70501ea2dce54ecb03936164ea4f5ccfab2
James Paul Turner <james.turner@imperial.ac.uk>
2025-01-23 16:36:34 +0000

    Log pruned memberships by default.

--- imperial_coldfront_plugin/management/commands/prune_groups.py
+++ imperial_coldfront_plugin/management/commands/prune_groups.py
@@ -1,6 +1,5 @@
 """Django management command to prune expired group memberships."""
 
-from argparse import ArgumentParser
 from typing import Any
 
 from django.core.management.base import BaseCommand
@@ -14,16 +13,8 @@ class Command(BaseCommand):
 
     help = __doc__
 
-    def add_arguments(self, parser: ArgumentParser) -> None:
-        """Add commandline options."""
-        parser.add_argument("--debug", action="store_true")
-
-    def handle(self, debug: bool = False, **kwargs: Any) -> None:  # type: ignore[misc]
+    def handle(self, *args: Any, **kwargs: Any) -> None:  # type: ignore[misc]
         """Command business logic."""
         query = GroupMembership.objects.filter(expiration__lt=timezone.now())
-
-        if debug:
-            self.stdout.write(f"Pruning {query.count()} expired group memberships.")
-
-        if query.count():
-            query.delete()
+        self.stdout.write(f"Pruning {query.count()} expired group memberships.")
+        query.delete()
