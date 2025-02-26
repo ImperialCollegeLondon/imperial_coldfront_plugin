e3e0f874e112001a8c4cff0d6b58d2a3fd73ccc2
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2025-02-26 06:21:36 +0000

    add unit test for the task

--- imperial_coldfront_plugin/tasks.py
+++ imperial_coldfront_plugin/tasks.py
@@ -10,7 +10,7 @@ from .models import GroupMembership
 
 def send_expiration_notifications():
     """Send expiration notifications to users whose memberships are about to expire."""
-    expiration_days = settings.MEMBERSHIP_EXPIRATION_NOTIFICATION_DAYS
+    expiration_days = settings.MEMBERSHIP_EXPIRATION_DAYS
     expiration_date = timezone.now() + timedelta(days=expiration_days)
     memberships = GroupMembership.objects.filter(expiration_date=expiration_date)
     for membership in memberships:
---
+++ tests/test_tasks.py
@@ -0,0 +1,30 @@
+"""Tests for the tasks of the Imperial Coldfront plugin."""
+
+import pytest
+from django.utils import timezone
+
+from imperial_coldfront_plugin.models import GroupMembership
+from imperial_coldfront_plugin.tasks import send_expiration_notifications
+
+
+@pytest.mark.django_db
+def test_send_expiration_notifications(settings, mailoutbox):
+    """Test the send_expiration_notifications task."""
+    # Create a group membership that is about to expire.
+    group_membership = GroupMembership(
+        expiration=timezone.now() + timezone.timedelta(days=1)
+    )
+
+    # Set the MEMBERSHIP_EXPIRATION_DAYS setting.
+    settings.MEMBERSHIP_EXPIRATION_DAYS = 1
+
+    # Call the task.
+    send_expiration_notifications()
+
+    # Check that the email was sent.
+    assert len(mailoutbox) == 1
+    email = mailoutbox[0]
+    assert email.subject == "HPC Access Expiration Alert"
+    assert group_membership.user.email in email.to
+    assert group_membership.group.owner.email in email.to
+    assert str(group_membership.expiration_date.date()) in email.body
