0f213b6b2050e191587daf3384837434f9c44947
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-03-04 15:57:57 +0000

    :sparkles: Add session and filesystems endpoint.

--- imperial_coldfront_plugin/gpfs_client.py
+++ imperial_coldfront_plugin/gpfs_client.py
@@ -1,5 +1,6 @@
 """Interface for interacting with the GPFS API."""
 
+import requests
 from django.conf import settings
 from uplink import Consumer, get
 from uplink.auth import BasicAuth
@@ -8,12 +9,12 @@ from uplink.auth import BasicAuth
 class GPFSClient(Consumer):
     """Client for interacting with the GPFS API."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialise the client with the base URL and authentication."""
+        session = requests.Session()
         auth = BasicAuth(settings.GPFS_API_USERNAME, settings.GPFS_API_PASSWORD)
-        super().__init__(base_url=settings.GPFS_API_URL, auth=auth)
+        super().__init__(base_url=settings.GPFS_API_URL, auth=auth, session=session)
 
-    @get("users/{username}")
-    def user_info(self, username: str):
-        """Get information about a user."""
-        pass
+    @get("filesystems")
+    def filesystems(self) -> str:
+        """Return the information on the filesystems available."""
