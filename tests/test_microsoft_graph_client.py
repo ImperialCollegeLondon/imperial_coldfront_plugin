feab63e21c95bec6b48d4e38bcae06bde82f40ea
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-03-05 06:46:44 +0000

    :white_check_mark: Add test_client_search_user.

--- tests/test_microsoft_graph_client.py
+++ tests/test_microsoft_graph_client.py
@@ -1,3 +1,4 @@
+import urllib
 from unittest.mock import MagicMock
 
 import pytest
@@ -83,3 +84,27 @@ def test_build_user_search_query(search_by, query):
     term = "username"
     actual = build_user_search_query(term, search_by)
     assert actual == query.format(term=term)
+
+
+@pytest.mark.parametrize(
+    "search_by,query",
+    [
+        (
+            "all_fields",
+            '"displayName:{term}" OR ("userPrincipalName:{term}" OR "mail:{term}")',
+        ),
+        ("something_else", '"userPrincipalName:{term}"'),
+    ],
+)
+def test_client_search_user(search_by, query, graph_client, parsed_profile, send_mock):
+    """Test that the client correctly retrieves and transforms user profile data."""
+    username = "testuser"
+    send_mock.return_value.json.return_value = dict(value=[])
+    graph_client.user_search_by(username, search_by)
+    expected = (
+        BASE_URL
+        + f"/users?$search={urllib.parse.quote(query.format(term=username))}&$select="
+        + PROFILE_ATTRIBUTES
+    )
+    send_mock.assert_called_once()
+    assert send_mock.call_args[0][0].url == expected
