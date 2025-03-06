d36bbded8fa085061b3d4bd15cec76d2c3fa5081
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-03-06 13:02:06 +0000

    Add view function to create rdf allocations

--- imperial_coldfront_plugin/settings.py
+++ imperial_coldfront_plugin/settings.py
@@ -14,4 +14,18 @@ GPFS_API_URL = ENV.str("GPFS_API_URL", default="")
 GPFS_API_USERNAME = ENV.str("GPFS_API_USERNAME", default="")
 GPFS_API_PASSWORD = ENV.str("GPFS_API_PASSWORD", default="")
 GPFS_API_VERIFY = ENV.bool("GPFS_API_VERIFY", default=False)
+
 EXPIRATION_NOTIFICATION_DAYS = [1, 5, 30]
+
+LDAP_USERNAME = ENV.str("LDAP_USERNAME", default="")
+LDAP_PASSWORD = ENV.str("LDAP_PASSWORD", default="")
+LDAP_URI = ENV.str("LDAP_URI", default="")
+LDAP_USER_OU = ENV.str(
+    "LDAP_USER_OU", default="OU=Users,OU=Imperial College (London),dc=ic,dc=ac,dc=uk"
+)
+LDAP_GROUP_OU = ENV.str(
+    "LDAP_GROUP_OU",
+    default="OU=RCS,OU=Groups,OU=Imperial College (London),DC=ic,DC=ac,DC=uk",
+)
+
+LDAP_ENABLED = bool(LDAP_USERNAME and LDAP_PASSWORD and LDAP_URI)
