e67c0aa6812a67a5bc4d4a437e11bade0926dc7b
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2025-03-06 15:46:10 +0000

    sort owner and member in test email

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -23,14 +23,8 @@ class TestSendExpirationNotificationsTask:
         send_expiration_notifications()
 
         email = mailoutbox[0]
-        email.subject == "HPC Access Expiration Alert"
-        email.to == [pi.email, pi_group_membership.group.owner.email]
-        email.body == (
-            f"This email is to notify you that {pi.get_full_name()} ({pi.email})'s "
-            f"membership in the HPC access group of "
-            f"{pi_group_membership.group.owner.get_full_name()} is due "
-            f"to expire on {pi_group_membership.expiration}."
-        )
+        assert email.subject == "HPC Access Expiration Alert"
+        assert email.to == [pi_group_membership.member.email, pi.email]
 
     def test_no_notification_sent_for_expired_membership(
         self, pi_group_membership, mailoutbox
