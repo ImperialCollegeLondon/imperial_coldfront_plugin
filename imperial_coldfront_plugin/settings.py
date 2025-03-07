787363ab69b0fad1e0d918f1809fa27d034cb781
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-03-07 12:28:57 +0000

    Add docs strings for LDAP config values

--- imperial_coldfront_plugin/settings.py
+++ imperial_coldfront_plugin/settings.py
@@ -18,14 +18,19 @@ GPFS_API_VERIFY = ENV.bool("GPFS_API_VERIFY", default=False)
 EXPIRATION_NOTIFICATION_DAYS = [1, 5, 30]
 
 LDAP_USERNAME = ENV.str("LDAP_USERNAME", default="")
+"""Active Directory user account name."""
 LDAP_PASSWORD = ENV.str("LDAP_PASSWORD", default="")
+"""Password for LDAP_USERNAME."""
 LDAP_URI = ENV.str("LDAP_URI", default="")
+"""Active Directory server URI."""
 LDAP_USER_OU = ENV.str(
     "LDAP_USER_OU", default="OU=Users,OU=Imperial College (London),dc=ic,dc=ac,dc=uk"
 )
+"""The organisational unit containing Imperial user data."""
 LDAP_GROUP_OU = ENV.str(
     "LDAP_GROUP_OU",
     default="OU=RCS,OU=Groups,OU=Imperial College (London),DC=ic,DC=ac,DC=uk",
 )
+"""The organisational unit containing RDF access groups."""
 
 LDAP_ENABLED = bool(LDAP_USERNAME and LDAP_PASSWORD and LDAP_URI)
