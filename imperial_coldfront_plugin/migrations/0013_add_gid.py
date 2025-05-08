c538f6ef76ba5ea9c43259097ff1d1b7067d55f7
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2025-05-08 08:54:31 +0100

    add GID via data migration

---
+++ imperial_coldfront_plugin/migrations/0013_add_gid.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+
+from coldfront.core.allocation.models import AllocationAttributeType, AttributeType
+
+
+def add_gid_attribute_type(apps, schema_editor):
+    """Add a private AllocationAttributeType of "GID" via data migration."""
+    AllocationAttributeType = apps.get_model("allocation", "AllocationAttributeType")
+    AttributeType = apps.get_model("allocation", "AttributeType")
+
+    text_attribute_type, _ = AttributeType.objects.get_or_create(name="Text")
+    AllocationAttributeType.objects.get_or_create(
+        name="GID",
+        defaults=dict(
+            attribute_type=text_attribute_type,
+            is_unique=True,
+            is_private=True,
+            is_changeable=False,
+        ),
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("imperial_coldfront_plugin", "0012_delete_unixuid"),
+    ]
+
+    operations = [
+        migrations.RunPython(add_gid_attribute_type),
+    ]
