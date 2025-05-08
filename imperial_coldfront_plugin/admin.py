78b39a0516f784ef87a8abca775dba4ff5be7596
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-05-08 13:39:43 +0100

    String out group related functionality

--- imperial_coldfront_plugin/admin.py
+++ imperial_coldfront_plugin/admin.py
@@ -1,19 +1,1 @@
 """Django admin configuration."""
-
-from django.contrib import admin
-
-from .models import GroupMembership, ResearchGroup
-
-
-@admin.register(ResearchGroup)
-class ResearchGroupAdmin(admin.ModelAdmin):
-    """Admin configuration for the ResearchGroup model."""
-
-    list_display = ("owner", "gid", "name")
-
-
-@admin.register(GroupMembership)
-class GroupMembershipAdmin(admin.ModelAdmin):
-    """Admin configuration for the GroupMembership model."""
-
-    pass
--- tests/test_policy.py
+++ tests/test_policy.py
@@ -1,6 +1,4 @@
 import pytest
-from django.core.exceptions import PermissionDenied
-from django.utils import timezone
 
 from imperial_coldfront_plugin.policy import (
     HPC_ACCESS_ALLOWED_ENTITY_TYPE,
@@ -8,9 +6,6 @@ from imperial_coldfront_plugin.policy import (
     PI_ALLOWED_TITLES,
     PI_DISALLOWED_DEPARTMENTS,
     PI_DISALLOWED_TITLE_QUALIFIERS,
-    check_group_owner_manager_or_superuser,
-    check_group_owner_or_superuser,
-    user_already_has_hpc_access,
     user_eligible_for_hpc_access,
     user_eligible_to_be_pi,
 )
@@ -61,61 +56,3 @@ def test_user_eligible_to_be_pi(job_title, pi_user_profile):
 def test_user_eligible_to_be_pi_invalid(override_key, override_value, pi_user_profile):
     """Test the pi filter catches invalid profiles."""
     assert not user_eligible_to_be_pi(pi_user_profile | {override_key: override_value})
-
-
-class TestCheckGroupOwnerManagerOrSuperuser:
-    """Tests for the check_group_owner_manager_or_superuser function."""
-
-    def test_pass(self, pi_group, pi_manager_or_superuser):
-        """Test the check passes for a group owner or manager or a superuser."""
-        check_group_owner_manager_or_superuser(pi_group, pi_manager_or_superuser)
-
-    def test_expired_manager(self, pi_group, pi_group_manager):
-        """Test the check fails for an expired manager."""
-        pi_group_manager.groupmembership.expiration = timezone.datetime.min
-        pi_group_manager.groupmembership.save()
-        with pytest.raises(PermissionDenied):
-            check_group_owner_manager_or_superuser(pi_group, pi_group_manager)
-
-    def test_user(self, pi_group, user_or_member):
-        """Test the check fails for other users."""
-        with pytest.raises(PermissionDenied):
-            check_group_owner_manager_or_superuser(pi_group, user_or_member)
-
-
-class TestCheckGroupOwnerOrSuperuser:
-    """Tests for the check_group_owner_or_superuser function."""
-
-    def test_pass(self, pi_group, pi_or_superuser):
-        """Check the group owner or superuser passes the check."""
-        check_group_owner_or_superuser(pi_group, pi_or_superuser)
-
-    def test_manager(self, pi_group, user_member_or_manager):
-        """Test the check fails for a other users."""
-        with pytest.raises(PermissionDenied):
-            check_group_owner_or_superuser(pi_group, user_member_or_manager)
-
-
-def test_user_already_has_hpc_access_no_user(db):
-    """Test if non-existent user already has access."""
-    assert not user_already_has_hpc_access("username")
-
-
-def test_user_already_has_hpc_access(user):
-    """Test if a user already has access."""
-    assert not user_already_has_hpc_access(user.username)
-
-
-def test_user_already_has_hpc_access_group_membership(pi_group_member):
-    """Test if a user with a group membership already has access."""
-    assert user_already_has_hpc_access(pi_group_member.username)
-
-
-def test_user_already_has_hpc_access_group_owner(pi_group):
-    """Test if a user with a group ownership already has access."""
-    assert user_already_has_hpc_access(pi_group.owner.username)
-
-
-def test_user_already_has_hpc_access_superuser(superuser):
-    """Test if a superuser already has access."""
-    assert user_already_has_hpc_access(superuser.username)
