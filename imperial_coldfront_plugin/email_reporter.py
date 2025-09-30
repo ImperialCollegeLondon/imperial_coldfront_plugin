3da6a6f8be4db61b61a1af2541a53c1fe660e033
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-09-30 14:07:22 +0100

    Add django q email reporter

---
+++ imperial_coldfront_plugin/email_reporter.py
@@ -0,0 +1,27 @@
+"""Email reporter for Coldfront system monitor."""
+
+import sys
+import traceback
+
+from django.conf import settings
+from django.core.mail import send_mail
+
+
+class EmailReporter:
+    """Simple email reporter for Coldfront system monitor."""
+
+    def report(self):
+        """Send an email report."""
+        _, _, tb = sys.exc_info()
+
+        tb_txt = "".join(traceback.format_tb(tb))
+
+        send_mail(
+            "Error in Coldfront job queue",
+            f"""An error occurred whilst processing a job.
+{tb_txt}
+            """,
+            settings.EMAIL_SENDER,
+            settings.EMAIL_ADMIN_LIST.split(","),
+            fail_silently=False,
+        )
