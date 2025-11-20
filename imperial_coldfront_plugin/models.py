40fb2a7c6b3b5baf314da5069c0399b726bab592
Sahil Raja <s.raja@imperial.ac.uk>
2025-11-20 16:25:07 +0000

    adding the transaction model

--- imperial_coldfront_plugin/models.py
+++ imperial_coldfront_plugin/models.py
@@ -1,1 +1,17 @@
 """Plugin Django models."""
+
+from coldfront.core.project.models import Project
+from django.db import models
+
+
+class CreditTransaction(models.Model):
+    """Model representing a credit transaction."""
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    amount = models.IntegerField()
+    description = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """String representation of the CreditTransaction."""
+        return f"CreditTransaction(id={self.id}, project={self.project.name})"
