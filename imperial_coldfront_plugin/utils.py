d3958514eee63524188dcf204c9b0cf486780350
Sahil Raja <s.raja@imperial.ac.uk>
2025-11-26 13:50:31 +0000

    adding credit balance function

--- imperial_coldfront_plugin/utils.py
+++ imperial_coldfront_plugin/utils.py
@@ -1,6 +1,10 @@
 """Utility functions for the Imperial Coldfront plugin."""
 
 from coldfront.core.allocation.models import Allocation, AllocationAttribute
+from coldfront.core.project.models import Project
+from django.db.models import Sum
+
+from imperial_coldfront_plugin.models import CreditTransaction
 
 
 def get_allocation_shortname(allocation: Allocation) -> str:
@@ -21,3 +25,18 @@ def get_allocation_shortname(allocation: Allocation) -> str:
         AllocationAttribute.DoesNotExist,
     ):
         return ""
+
+
+def calculate_credit_balance(project: Project) -> int:
+    """Return the summed credit balance for a project.
+
+    Args:
+        project: The project whose credit balance is to be calculated.
+
+    Returns:
+        The total credit balance as an integer, or None if there are no transactions.
+    """
+    result = CreditTransaction.objects.filter(project=project).aggregate(
+        total=Sum("amount")
+    )["total"]
+    return result
