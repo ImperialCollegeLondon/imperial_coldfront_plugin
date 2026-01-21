102ba0f9a4a8be17c539dddaac2a26482e770967
sahil raja <sahilraja100@gmail.com>
2026-01-21 12:11:07 +0000

    scheduling task

--- imperial_coldfront_plugin/apps.py
+++ imperial_coldfront_plugin/apps.py
@@ -5,6 +5,7 @@ This is part of the boilerplate needed for a Django app.
 
 import django_stubs_ext
 from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
 
 
 class ImperialColdfrontPluginConfig(AppConfig):
@@ -16,6 +17,7 @@ class ImperialColdfrontPluginConfig(AppConfig):
     def ready(self) -> None:
         """Wire up signal handlers for app."""
         from django.conf import settings
+        from django_q.models import Schedule
 
         from . import signals  # noqa: F401
         from .gid import validate_gid_ranges
@@ -25,3 +27,16 @@ class ImperialColdfrontPluginConfig(AppConfig):
         # Ensure GID_RANGES setting is valid
         # do it here to avoid circular import issues
         validate_gid_ranges(settings.GID_RANGES)
+
+        try:
+            Schedule.objects.get_or_create(
+                name="RDF allocation notifications",
+                defaults=dict(
+                    func="imperial_coldfront_plugin.tasks.send_rdf_allocation_notifications_task",
+                    schedule_type=Schedule.DAILY,
+                    repeats=-1,
+                ),
+            )
+        except (OperationalError, ProgrammingError):
+            # DB not ready (e.g., during migrate)
+            pass
