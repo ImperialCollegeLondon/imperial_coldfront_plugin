fb757a4c6e86790a39b58c05a593eab3ca4691eb
Sahil Raja <sraja@ic.ac.uk>
2026-02-26 12:13:55 +0000

    add custom template tag and update template to use

---
+++ imperial_coldfront_plugin/templatetags/allocation-days.py
@@ -0,0 +1,15 @@
+"""Template filter for calculating days until a given date."""
+
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+
+
+@register.filter
+def days_until(date_value):
+    """Calculate days until the given date from today."""
+    if date_value:
+        days = (date_value - timezone.now().date()).days
+        return max(0, days)
+    return None
