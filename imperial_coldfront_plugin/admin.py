f3b12a693ec5c116f1734ce0cccaca22e438b525
Sahil Raja <sraja@ic.ac.uk>
2026-04-16 10:24:23 +0100

    add credit transaction to admin

--- imperial_coldfront_plugin/admin.py
+++ imperial_coldfront_plugin/admin.py
@@ -1,1 +1,12 @@
 """Django admin configuration."""
+
+from django.contrib import admin
+
+from .models import CreditTransaction
+
+
+@admin.register(CreditTransaction)
+class CreditTransactionAdmin(admin.ModelAdmin):
+    """Admin configuration for CreditTransaction model."""
+
+    list_display = ("authoriser", "project", "timestamp", "amount", "description")
