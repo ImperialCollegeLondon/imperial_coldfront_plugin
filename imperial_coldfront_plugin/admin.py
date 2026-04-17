d5bcc8ca12fe3cc17caab697facab770b4984383
Sahil Raja <56438860+Sahil590@users.noreply.github.com>
2026-04-17 12:27:28 +0100

    Update imperial_coldfront_plugin/admin.py
    Co-authored-by: Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>

--- imperial_coldfront_plugin/admin.py
+++ imperial_coldfront_plugin/admin.py
@@ -6,7 +6,7 @@ from .models import CreditTransaction
 
 
 @admin.register(CreditTransaction)
-class CreditTransactionAdmin(admin.ModelAdmin):
+class CreditTransactionAdmin(admin.ModelAdmin[CreditTransaction]):
     """Admin configuration for CreditTransaction model."""
 
     list_display = ("authoriser", "project", "timestamp", "amount", "description")
